7e28310857eaf834b419fa1445bb3c257c6da261
Shreshtha-ai <shreshthaagarwal2007@gmail.com>
2026-06-23 13:32:47 +0530

    feat: initialize React project with Vite, Appwrite authentication service, and core configuration

---
+++ Scriptiq/src/appwrite/auth.js
@@ -0,0 +1,46 @@
+import conf from "../config/config";
+import {Client , Account, ID} from "appwrite";
+
+  export class AuthService{
+    client = new Client();
+    account;
+
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+            this.account = new Account(this.client)
+    }
+
+    async createAccount({email , password , name}){
+        try{
+            const userAcoount = await this.account.create(ID.unique(), email, password, name);
+            if(userAcoount){
+                // call another method
+            }
+            else{
+                return userAcoount;
+            }
+
+        }
+        catch(error){
+            throw error;
+        }
+    }
+    async login({email, password}){
+        try{
+            await this.account.createEmailPasswordSession(email,password);
+            
+        }
+        catch(error){
+            throw error;
+        }
+
+    }
+
+
+            
+    }
+
+  
+
