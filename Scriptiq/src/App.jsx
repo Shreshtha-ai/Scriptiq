25cb589496fc5af6252113827fd26738a86bae4a
Shreshtha-ai <shreshthaagarwal2007@gmail.com>
2026-06-24 15:19:17 +0530

    feat:Replace default CSS with Tailwind import in index.css

--- Scriptiq/src/App.jsx
+++ Scriptiq/src/App.jsx
@@ -1,9 +1,42 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import { logout, login } from './store/authSlice'
+import authService from './appwrite/auth'
+import { Header, Footer } from './compoenents'
+import { Outlet } from 'react-router-dom'
+
 
 function App() {
-  return (
-    <h1>A blog app with appwrite </h1>
-  )
+  const [loading, setLoading] =  useState(true)
+  const dispatch = useDispatch()
+
+  //load user data from database after login
+// it shows 401 error if there are no users data, so that we put this 
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))                   
+  })
+  
+  return !loading? ( 
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-500'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+       
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App
