0df432927ac143cd4dc463279f86a2d3a932dfec
Shreshtha-ai <shreshthaagarwal2007@gmail.com>
2026-06-28 18:40:42 +0530

    feat: implement Signup page and add to component barrel file

--- Scriptiq/src/compoenents/index.js
+++ Scriptiq/src/compoenents/index.js
@@ -1,11 +1,23 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import  Container from "./container/container";
+import Container from "./container/container";
 import Logo from "./Logo/Logo";
+import Button from "./Button";
+import Input from "./Input";
+import RTE from "./RTE";
+import LogoutBtn from "./LogoutBtn";
+import Signup from "./Signup";
+
+
 
 export {
     Header,
     Footer,
     Container,
-    Logo
+    Logo,
+    Button,
+    Input,
+    RTE,
+    LogoutBtn,
+    Signup
 }
